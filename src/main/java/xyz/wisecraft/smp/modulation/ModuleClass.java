66419120f10c5585123c5290ad35f9238ad05c1c
GZimmer <kristianz1000@gmail.com>
2023-07-11 18:12:48 +0200

    Added javadocs

--- src/main/java/xyz/wisecraft/smp/modulation/ModuleClass.java
+++ src/main/java/xyz/wisecraft/smp/modulation/ModuleClass.java
@@ -8,19 +8,39 @@ public interface ModuleClass {
 
     int id = -1;
 
+    /**
+     * This method is called when the module is enabled.
+     */
     default void startModule() {
         onEnable();
         registerEvents();
         registerCommands();
     }
 
+    /**
+     * This method is called when the module is disabled.
+     */
     default void stopModule() {
         onDisable();
     }
 
+    /**
+     * This method should have all the startup code for the module.
+     */
     void onEnable();
 
+    /**
+     * This method should have all the shutdown code for the module.
+     */
     default void onDisable() {}
+
+    /**
+     * This method should register all the events for the module.
+     */
     default void registerEvents() {}
+
+    /**
+     * This method should register all the commands for the module.
+     */
     default void registerCommands() {}
 }
